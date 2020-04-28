e026578da4e42bbb7287c3c734cfe320382ac787
Adib-LGS <58850596+Adib-LGS@users.noreply.github.com>
2020-04-28 19:03:59 +0000

    Update Database.php

--- libraries/Database.php
+++ libraries/Database.php
@@ -11,7 +11,7 @@ class Database {
     public static function getPdo():PDO
     {
 
-    $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', 'root', [
+    $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', '*******', '******', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
